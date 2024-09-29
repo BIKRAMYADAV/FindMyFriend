149fa08c247bf987d431a0a3992cd515aac2deb6
bikram11git <bikram@myparticipants.com>
2024-09-29 20:13:21 +0530

    feat: added a router for adding friend

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 const { mongoConnect } = require('./configs/mongoConnection');
 dotenv.config()
 const PORT = process.env.PORT
+const router = require('./routes/addFriend')
 
 const app = express();
+
+app.use(router);
 
 app.listen(PORT, () => {
     mongoConnect();
