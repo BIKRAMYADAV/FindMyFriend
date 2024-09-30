cf1d8ede562acc0aafed3e8d7a0ce26dc24b33b1
bikram11git <bikram@myparticipants.com>
2024-09-30 14:06:44 +0530

    feat: added two new functions to add and remove friend from the repository

--- backend/src/DB/repository/friend-repository.js
+++ backend/src/DB/repository/friend-repository.js
@@ -2,7 +2,55 @@ const friendModel = require('../models/friendModel')
 
 class friendRepository {
     async AllFriends() {
+            try {
+                const data = await friendModel.find();
+                return data;
+            } catch (error){
+                console,error('There was an error in fetching all the friends', error);
+                throw error;
+            }
+    }
+    async AddFriend(friend) {
+        try {
+            const {name, id} = friend;
+            const existingFriend = await friendModel.findOne({name,id});
+            if (existingFriend){
+                return {
+                    status: "409",
+                    message: `${name} already exists as a friend`
+                }
+            }
+            const response = new friendModel({...friend});
+            await response.save();
+            return {
+                status: "200",
+                message: "friend added succesfully"
+            }
+        } catch (error){
+            console.error('There was an error in adding a new friend', error);
+            throw error;
+        }
+    }
 
+    async deleteFriend(friend){
+        try {
+            const {name, id} = friend;
+            const response = friendModel.find({name,id});
+            if (!response){
+                throw new error('The friend does not exist in your list');
+            }
+            const deletedFriend = await friendModel.findByIdAndDelete(id);
+            if (!deletedFriend){
+                throw new error('The friend does not exist on the database');
+            }
+            return {
+                status: 200,
+                message : "The friend was removed succesfully"
+            }
+        } catch (error){
+            console.error('There was an error in removing the friend', error);
+            throw error;
+        }
     }
 }
 
--- backend/src/api/addFriend.js
+++ backend/src/api/addFriend.js
@@ -1,17 +1,4 @@
-// const express = require('express');
-// const { addFriend } = require('../services/addFriend');
-// const router = express.Router();
 
-// router.get('friends', (req, res) => {
-//     res.send("Here is the list of the friends");
-// })
-
-// router.post('/add-friend', (req, res) => {
-//     const friend = req.body;
-//     addFriend(friend);
-// })
-
-// module.exports = router;
 module.exports = (app) => {
     app.post('/add-friend', (req, res, next) => {
         
